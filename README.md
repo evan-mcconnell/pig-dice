51fffba711d778c1d16f560f2130c69408381226
katy and Kimberly McConnell <katy@>
2018-10-22 20:35:36 -0700

    fun pig dice game that totally works

---
+++ README.md
@@ -0,0 +1,26 @@
+
+
+#### Generate random numbers for dice roll
+ _Click Roll_
+
+ OUTPUT: generate a number 1-6
+
+#### If a 1 is rolled, turn score is 0 and turn is ended
+_Player 1 rolls 1_
+
+OUTPUT: Player 1 turn score = 0
+
+#### If 2-6 is rolled, add number to previous turn score
+_If a 4 and 5 are rolled_
+
+OUTPUT: 9
+
+#### Hold will add turn score to total score1
+_Click Hold_
+
+OUTPUT: Add turn score to total score
+
+#### Is score total is >= 100, that player wins
+_Player 1 has 95 pts, rolls and keep 5_
+
+OUTPUT: player 1 wins!
--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,96 @@
 // biz logic
-var score1 = 0;
-var score2 = 0;
-var turn = 0;
-var turnTotal1 = 0;
-var rollTotal1 = 0;
-var turnTotal2 = 0;
-var rollTotal2 = 0;
 
-var rollTurn = function (roll) {
-  roll = Math.floor(Math.random() * 6) + 1;
-  if (roll > 1) {
-    turn += roll;
+// var score1 = 0;
+// var score2 = 0;
+// var turn = 0;
+// var turnTotal1 = 0;
+// var rollTotal1 = 0;
+// var turnTotal2 = 0;
+// var rollTotal2 = 0;
+
+// var rollTurn = function(roll) {
+//roll = Math.floor(Math.random() * 6) + 1;
+//   if (roll > 1) {
+//     turn += roll;
+//   } else {
+//     turn = 0;
+//   };
+//   alert(roll);
+//   return turn;
+// };
+
+function Players(roll, turn, score) {
+  this.roll = roll;
+  this.turn = turn;
+  this.score = score;
+}
+
+Players.prototype.rollDice = function() {
+  this.roll = Math.floor(Math.random() * 6) + 1;
+}
+
+Players.prototype.turnScore = function() {
+  if (this.roll > 1) {
+    this.turn += this.roll;
   } else {
-    turn = 0;
+    this.turn = 0;
   };
-  return turn;
+  alert(this.roll);
+  return this.turn;
 };
 
+Players.prototype.hold = function() {
+  this.score += this.turn;
+  return this.score;
+};
+
+// var computerTurn = function() {
+//   for (var i = 0; i < 3; i++) {
+//     roll = Math.floor(Math.random() * 6) + 1;
+//     turn += roll;
+//     return turn;
+//     console.log(turn);
+//   };
+// };
+
 // user logic
-$(document).ready(function(){
-  $(".roll1").click(function(event){
+// Player 1 Rolling
+$(document).ready(function() {
+
+  var player1 = new Players (0,0,0);
+
+  $(".roll1").click(function(event) {
     event.preventDefault();
-    turnTotal1 = rollTurn(rollTotal1);
-    $("#turnScore1").text(turnTotal1);
+    player1.rollDice();
+    player1.turnScore();
+    console.log(player1.turn);
+    // turnTotal1 = rollTurn(rollTotal1);
+    $("#turnScore1").text(player1.turn);
   });
 
-  $(".hold1").click( function(event) {
+  $(".hold1").click(function(event) {
     event.preventDefault();
-    score1 =+ turnTotal1
-    $("#scoreOne").text(score1);
-    rollTotal1 = 0;
-    rollTotal2 = 0;
-    turnTotal1 = 0;
-    turnTotal2 = 0;
-  })
+    player1.hold();
+    player1.turn = 0;
+    $("#turnScore1").text(player1.turn);
+    $("#scoreOne").text(player1.score);
+    // turn = 0;
+    // score1 += turnTotal1
+    // $("#scoreOne").text(score1);
+    // rollTotal1 = 0;
+    // turnTotal1 = 0;
+    // $("#turnScore1").text(turnTotal1);
+  });
+
+  // //Player 2 Rolling (Computer)
+  // $(".roll2").click(function(event) {
+  //   for (var i = 0; i < 3; i++) {
+  //     roll = Math.floor(Math.random() * 6) + 1;
+  //     turn += roll;
+  //     return turn;
+  //     console.log(turn);
+  //   };
+  //   // return computerTurn();
+    // console.log(rollTotal2);
+  // });
 });
