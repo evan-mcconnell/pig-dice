450cdaf7b01b569b26a0ef5061b8274fe41592bb
Kimberly McConnell <kmcconnell393@gmail.com>
2018-10-22 17:17:47 -0700

    allow score to print to top

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,37 @@
 // biz logic
-var turnTotal = 0;
-var rollTotal = 0;
+var score1 = 0;
+var score2 = 0;
+var turn = 0;
+var turnTotal1 = 0;
+var rollTotal1 = 0;
+var turnTotal2 = 0;
+var rollTotal2 = 0;
 
+var rollTurn = function (roll) {
+  roll = Math.floor(Math.random() * 6) + 1;
+  if (roll > 1) {
+    turn += roll;
+  } else {
+    turn = 0;
+  };
+  return turn;
+};
 
 // user logic
 $(document).ready(function(){
-  $(".roll").click(function(event){
+  $(".roll1").click(function(event){
     event.preventDefault();
-    rollTotal = Math.floor(Math.random() * 6) + 1;
-    if (rollTotal > 1) {
-      turnTotal += rollTotal;
-    } else {
-      turnTotal = 0;
-    }
-    console.log(turnTotal)
+    turnTotal1 = rollTurn(rollTotal1);
+    $("#turnScore1").text(turnTotal1);
   });
+
+  $(".hold1").click( function(event) {
+    event.preventDefault();
+    score1 =+ turnTotal1
+    $("#scoreOne").text(score1);
+    rollTotal1 = 0;
+    rollTotal2 = 0;
+    turnTotal1 = 0;
+    turnTotal2 = 0;
+  })
 });
