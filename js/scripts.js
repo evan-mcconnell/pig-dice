83ed6e39aea50af8342f9b78318a13cb74c97882
katy and Kimberly McConnell <kimberly@>
2018-10-24 18:33:36 -0700

    make player 2 work and add some nice styling

--- js/scripts.js
+++ js/scripts.js
@@ -39,6 +39,7 @@ Players.prototype.turnScore = function() {
   return this.turn;
 };
 
+
 Players.prototype.hold = function() {
   this.score += this.turn;
   return this.score;
@@ -58,13 +59,13 @@ Players.prototype.hold = function() {
 $(document).ready(function() {
 
   var player1 = new Players (0,0,0);
+  var player2 = new Players (0,0,0);
 
   $(".roll1").click(function(event) {
     event.preventDefault();
     player1.rollDice();
     player1.turnScore();
     console.log(player1.turn);
-    // turnTotal1 = rollTurn(rollTotal1);
     $("#turnScore1").text(player1.turn);
   });
 
@@ -74,23 +75,21 @@ $(document).ready(function() {
     player1.turn = 0;
     $("#turnScore1").text(player1.turn);
     $("#scoreOne").text(player1.score);
-    // turn = 0;
-    // score1 += turnTotal1
-    // $("#scoreOne").text(score1);
-    // rollTotal1 = 0;
-    // turnTotal1 = 0;
-    // $("#turnScore1").text(turnTotal1);
   });
 
-  // //Player 2 Rolling (Computer)
-  // $(".roll2").click(function(event) {
-  //   for (var i = 0; i < 3; i++) {
-  //     roll = Math.floor(Math.random() * 6) + 1;
-  //     turn += roll;
-  //     return turn;
-  //     console.log(turn);
-  //   };
-  //   // return computerTurn();
-    // console.log(rollTotal2);
-  // });
+  $(".roll2").click(function(event) {
+    event.preventDefault();
+    player2.rollDice();
+    player2.turnScore();
+    console.log(player2.turn);
+    $("#turnScore2").text(player2.turn);
+  });
+
+  $(".hold2").click(function(event) {
+    event.preventDefault();
+    player2.hold();
+    player2.turn = 0;
+    $("#turnScore2").text(player2.turn);
+    $("#scoreTwo").text(player2.score);
+  });
 });
